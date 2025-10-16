a6ad74f28596e7b139ede5d4c38f32c20a30ad16
Joan Roca <joanroca@inspladelestany.cat>
2025-10-16 18:53:56 +0200

    tercer commit

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,6 @@ void main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
     }
-    System.out.println("Editat per mi, en Biel");
+    System.out.println("Editat per mi, en joan");
     // Ha estat editat per el col·laborador Biel Viñas
 }
