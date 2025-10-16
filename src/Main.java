bc9658446d9af6165e13b737f3dc6d707b12a24d
Biel Viñas <bielvinas@inspladelestany,cat>
2025-10-16 18:37:29 +0200

    Segon commit

--- src/Main.java
+++ src/Main.java
@@ -10,4 +10,6 @@ void main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
     }
+    System.out.println("Editat per mi, en Biel");
+    // Ha estat editat per el col·laborador Biel Viñas
 }
